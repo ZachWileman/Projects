0e3f2d74bc03f455586dad818a47ede6af070aba
Zachary Wileman <zdw27f@mst.edu>
2016-07-09 11:15:56 -0500

    Changes made:
    -Text editor can now clear the text box
    -Files can now be opened in the text editor

--- Text/Text Editor/main.py
+++ Text/Text Editor/main.py
@@ -3,10 +3,43 @@
 
 from tkinter import * # Module used for creating GUI
 import tkinter.scrolledtext as tkst
+from tkinter.filedialog import askopenfilename
+import sys
 
 #Temporary function for different commands
 def doNothing():
     print('do nothing function')
+
+# Exit function
+def exitProgram():
+    # *********************Need to add check for if user wants to save file
+    pass
+
+# Open file function
+def openFile():
+    fileName = askopenfilename(
+        filetypes=[('All Files', '*.*')],
+        title = 'Choose A File'
+        )
+
+    # SIDE-NOTE: 'end-1c' refers to one character back from END. END points to
+    # just beyond the last character in the text string, this expression refers
+    # to the last character itself. The -1c extension effectively strips the
+    # trailing \n that this widget adds to its contents (and which may add a
+    # blank line if saved in a file). Source: [O`Reilly] - Programming Python,
+    # 4th ed. For this function, END+'-1c' or 'end-1c' do the same thing.
+    if len(textBox.get('1.0', 'end-1c')):
+        textBox.delete('1.0', END)
+        #*********** Need to add check for if there is a file open before it
+        #*********** clears the text editor.
+
+    if fileName:
+        try:
+            with open(fileName) as f:
+                contents = f.read()
+                textBox.insert('1.0', contents)
+        except:
+            print('An error occured when trying to open the file.')
  
 # Creates the window & title
 window = Tk()
@@ -20,11 +53,11 @@ window.config(menu=menuBar)
 fileMenu = Menu(menuBar, tearoff=0)
 menuBar.add_cascade(label='File', menu=fileMenu)
 fileMenu.add_command(label='New', command=doNothing)
-fileMenu.add_command(label='Open', command=doNothing)
+fileMenu.add_command(label='Open', command=openFile)
 fileMenu.add_command(label='Save', command=doNothing)
 fileMenu.add_command(label='Save as...', command=doNothing)
 fileMenu.add_separator()
-fileMenu.add_command(label='Exit', command=doNothing)
+fileMenu.add_command(label='Exit', command=exitProgram)
 
 # Creating Edit submenu
 editMenu = Menu(menuBar, tearoff=0)
